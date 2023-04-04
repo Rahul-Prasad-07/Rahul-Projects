c0724cee062d9e903eed214476c035d028e66889
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-04-04 14:54:14 +0530

    Building Project work section

---
+++ portfolio-site-V1/src/component/Projects/ProjectData.js
@@ -0,0 +1,86 @@
+const workdata =[
+
+    {
+        codeLink: "https://github.com/IAmTamal/ShopCoders",
+        description:
+          "An Open source e-commerce application on the way to help all the devs out there get affordable yet authentic swags.",
+        imgUrl: {
+          _type: "image",
+          asset: {
+            _ref: "image-7681f6a3a9d4919f406110ab5a213723c77ccfbc-6912x3456-png",
+            _type: "reference",
+          },
+        },
+        projectLink: "https://shop-coders.vercel.app/",
+        tags: ["ecommerce", "Next JS", "Web App"],
+        title: "ShopCoders",  
+    },
+
+    {
+        codeLink: "https://github.com/IAmTamal/Aviate-BlocBit",
+    description:
+      "BlocBit is The ultimate place to learn everything you need to get started about Blockchains, Crypto and Bitcoins. ",
+    imgUrl: {
+      _type: "image",
+      asset: {
+        _ref: "image-d03e4aac4e87d2d148f55fab8ed787514bc96bbc-1341x695-png",
+        _type: "reference",
+      },
+    },
+    projectLink: "https://blocbit.vercel.app/",
+    tags: ["UI/UX", "React JS", "Web App"],
+    title: "Blocbit",
+    },
+
+    {
+        codeLink: "https://github.com/IAmTamal/appwrite-docker-wallHub",
+    description:
+      "WallHub is a webapp made with Appwrite, Docker  used to get acess to all the best quality Ultra HD wallpapers for all your screens !!",
+    imgUrl: {
+      _type: "image",
+      asset: {
+        _ref: "image-ae56bd1f355e6a844176454d065e049563cfcd39-1365x767-png",
+        _type: "reference",
+      },
+    },
+    projectLink: "https://wall-hub.vercel.app/",
+    tags: ["Docker", "UI/UX"],
+    title: "WallHub",
+    },
+
+    {
+        codeLink: "https://github.com/IAmTamal/step-into-next",
+    description:
+      " Facing problem with some stuffs and finding hard to find out the solution for it  ? Well, Here is the right place to find one !!",
+    imgUrl: {
+      _type: "image",
+      asset: {
+        _ref: "image-ada84a2416b583df30f01960273d89e712b573e8-1352x672-png",
+        _type: "reference",
+      },
+    },
+    projectLink: "https://issue-hunter.vercel.app/",
+    tags: ["Next JS", "UI/UX", "Web App"],
+    title: "Issue Hunter",
+
+    },
+
+    {
+        codeLink: "https://github.com/IAmTamal/Milan",
+    description:
+      "We aim at bringing all the various NGOs, donors under one single roof to ease the burden of finding appropriate donors, communities.",
+    imgUrl: {
+      _type: "image",
+      asset: {
+        _ref: "image-4fe9f2c500b118a2447aa2acb069088bc2e6a993-6912x3456-png",
+        _type: "reference",
+      },
+    },
+    projectLink: "https://milaan.vercel.app/",
+    tags: ["React JS", "Web App"],
+    title: "Milan",
+    },
+   
+]
+
+export default ProjectData;
--- portfolio-site-V1/src/component/Projects/Projects.jsx
+++ portfolio-site-V1/src/component/Projects/Projects.jsx
@@ -8,10 +8,35 @@ export const Projects = () => {
     const [activeFilter, setActiveFilter] = useState('All');
     const [animatedCard, setAnimatedCard] = useState({y:0, opacity:1}); 
 
+
+    const handleWorkFilter =(item)=>{
+      setActiveFilter(item);
+      setAnimatedCard([{y:100, opacity:0}]);
+
+      setTimeout(()=>{
+
+        setAnimatedCard([{y:0, opacity:1}]);
+
+        if(item ==='All'){
+          setFilterWork(works);
+        }else{
+          setfilterWork(works.filter((work)=>work.tags.includes(item)));
+        }
+      },500);
+    }
+
   return (
     
     <>
        <h2 className='head-textt'> My Creative <span>Portfolio</span> Section </h2>
+
+       <div className="app__work-filter">
+        {[ 'DevOps','Web App', 'React JS', 'Web3 Dapp', 'Solidity', 'Blockchain','All' ].map((item, index)=>(
+          <div key={index} onClick={()=>handleWorkFilter(item)} className={`app__work-filter-item app__flex p-text ${activeFilter === item? 'item-active':''}`}>
+            {item}
+          </div>
+        ))}
+       </div>
     
     </>
   )
